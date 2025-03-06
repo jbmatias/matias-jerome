2275a8e83b4087090e4710094021d33929f3ae70
jerome matias <jeromematias@jeromes-MacBook-Pro-2.local>
2025-03-06 19:22:08 +0800

    initial api and frontend files

---
+++ frontend/vite.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port:
+      process.env.PORT ||
+      process.env.npm_package_config_vite_port ||
+      3000,
+  },
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
