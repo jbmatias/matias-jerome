9f9047c217fe2032d72548373452884549ee0880
jerome matias <jeromematias@jeromes-MacBook-Pro-2.local>
2025-03-12 00:40:30 +0800

    put route name

--- api/routes/api.php
+++ api/routes/api.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\GetStoreHoursController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/store-hours', GetStoreHoursController::class)->name('index.store-hours');
-Route::put('/store-hours', ConfigurationController::class)->name('update.configure-hours');
+Route::put('/store-hours', ConfigurationController::class)->name('update.store-hours');
